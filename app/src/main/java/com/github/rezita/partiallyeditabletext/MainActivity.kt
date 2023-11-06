6b68b397c5bb9dfdf272548fe04e67365599b6e7
unknown <zita.reiner@gmail.com>
2023-11-06 20:51:29 +0000

    add textchangelistener for sample app

--- app/src/main/java/com/github/rezita/partiallyeditabletext/MainActivity.kt
+++ app/src/main/java/com/github/rezita/partiallyeditabletext/MainActivity.kt
@@ -2,22 +2,29 @@ package com.github.rezita.partiallyeditabletext
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
-import androidx.constraintlayout.widget.ConstraintLayout
 import com.github.rezita.partiallyeditabletext.PartiallyEditableText as PartiallyEditableText
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        initView()
     }
 
-    fun initView() {
+    override fun onStart() {
+        super.onStart()
+        initView()
+        setListeners()
+    }
+
+    private fun initView() {
         val btn = findViewById<Button>(R.id.button)
         btn.setOnClickListener { showResponseDialog() }
     }
@@ -32,8 +39,8 @@ class MainActivity : AppCompatActivity() {
         ).show()
     }
 
-    fun showResponseDialog() {
-        val editText = findViewById<PartiallyEditableText>(R.id.editable);
+    private fun showResponseDialog() {
+        val editText = findViewById<PartiallyEditableText>(R.id.editable)
         val typed = editText.editableText
         val builder: AlertDialog.Builder = AlertDialog.Builder(this)
         builder
@@ -43,5 +50,20 @@ class MainActivity : AppCompatActivity() {
             }
         val dialog: AlertDialog = builder.create()
         dialog.show()
+    }
+
+    private fun setListeners(){
+        val partiallyEditText = findViewById<PartiallyEditableText>(R.id.editable)
+        partiallyEditText.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                Toast.makeText(applicationContext, "Typed: ${partiallyEditText.editableText}", Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 }
